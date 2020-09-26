f55961d917fbc2a2a5cc9c1d917a56e7aec86d2b
Adrien <luanadriendev@gmail.com>
2020-09-26 16:24:38 -0300

    first

--- server.js
+++ server.js
@@ -3,11 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res)=>{
-    const jsona = {
-        "cavalo":"tendi vadia",
-        "nude":"agora"
-    }
-    res.json(jsona.cavalo)
+
 })
 
 app.listen(3001)
