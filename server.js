89f2a9a60c15fb11376ed967c0c8e58ecbac221f
Adrien <luanadriendev@gmail.com>
2020-09-26 16:43:57 -0300

    json

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require('express')
 
 const app = express()
 
+var port = process.env.PORT || 8080
+
 app.get('/', (req, res)=>{
     res.json({"cavalo":"opa"})
 })
 
-app.listen(8080)
+app.listen(port)
